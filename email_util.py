194d6f795b18bc055e0697a25bdc5016e196def5
Jeff Crum <jeffrey.crum81@gmail.com>
2025-06-12 10:24:01 -0700

    removed secrets from file and moved into config

--- email_util.py
+++ email_util.py
@@ -1,13 +1,18 @@
+import configparser
 import smtplib
 import ssl
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def send_email(user_message):
     host = "smtp.gmail.com"
     port = 465
-    username = "jeffrey.crum81@gmail.com"
-    password = "boochoazmttcgpey"
-    receiver = "jeffrey.crum81@gmail.com"
+    username = config['Settings']['email']
+    password = config['Settings']['password']
+    receiver = config['Settings']['email']
+    print(username)
     my_context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
